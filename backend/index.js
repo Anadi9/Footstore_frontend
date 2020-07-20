be03610139d96c01d4b034d404c41ef5d8dffd3c
Anadi9 <anadithakur99@gmail.com>
2020-07-20 10:33:11 +0530

    routes for auth, user, product, category added in backend

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,47 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const expressValidator = require('express-validator');
 require("dotenv").config();
+//import routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
 
 
 const app = express();
 app.use(express.json());
+
+//mongoDb setup
+mongoose.connect( process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}).then(() => console.log(`DB connected`));
+
+//middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
 app.use(cors());
 
-//setup routes
-app.use('/users', require('./routes/userRouter'));
+//routes middleware
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', productRoutes);
 
 
-//setup mongoose
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-}, (err) => {
-    if(err) throw err;
-    console.log('MongoDB connection established');
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`App is listening on port ${PORT}`);
-});
